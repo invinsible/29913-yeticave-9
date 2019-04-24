003c74134d88ce9252faaa992f24971752cc6ed3
Igor Sergeev <igor@tomatstudio.ru>
2019-04-24 10:00:13 +0300

    Добавление внешних ключей к полям в БД

--- schema.sql
+++ schema.sql
@@ -9,7 +9,6 @@ CREATE TABLE categories (
   name CHAR(68),
   code CHAR(68)  
 );
-CREATE UNIQUE INDEX catName ON categories(name);
 
 CREATE TABLE lots (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -20,20 +19,17 @@ CREATE TABLE lots (
   price INT,
   date_end DATE,
   cost INT,
-  user INT,
-  winner INT,
-  category INT
+  user_id INT,
+  winner_id INT,
+  category_id INT
 );
-CREATE INDEX lotName ON lots(name);
-CREATE INDEX lotPrice ON lots(price);
-CREATE INDEX lotCategory ON lots(category);
 
 CREATE TABLE costs (
   id INT AUTO_INCREMENT PRIMARY KEY,
   date_create DATE,
   price INT,
-  user INT,
-  category INT  
+  user_id INT,
+  lot_id INT  
 );
 
 CREATE TABLE users (
@@ -44,11 +40,24 @@ CREATE TABLE users (
   password CHAR(126),
   avatar CHAR(255),
   contacts TEXT,
-  lot INT,
-  cost INT  
+  lot_id INT,
+  cost_id INT  
 );
+
 CREATE UNIQUE INDEX email ON users(email);
 CREATE INDEX userName ON users(name);
+CREATE UNIQUE INDEX catName ON categories(name);
+CREATE INDEX lotName ON lots(name);
+CREATE INDEX lotPrice ON lots(price);
+CREATE INDEX lotCategory ON lots(category_id);
+ALTER TABLE lots ADD FOREIGN KEY (category_id) REFERENCES categories (id);
+ALTER TABLE lots ADD FOREIGN KEY (user_id) REFERENCES users (id);
+ALTER TABLE lots ADD FOREIGN KEY (winner_id) REFERENCES users (id);
+ALTER TABLE costs ADD FOREIGN KEY (user_id) REFERENCES users (id);
+ALTER TABLE costs ADD FOREIGN KEY (lot_id) REFERENCES lots (id);
+ALTER TABLE users ADD FOREIGN KEY (lot_id) REFERENCES lots (id);
+ALTER TABLE users ADD FOREIGN KEY (cost_id) REFERENCES costs (id);
+
 
 INSERT INTO categories
 SET name = 'Доски и лыжи', code = 'boards';
