71dfac158c01bb2ea3bbdac1a9194ae38a948413
Игорь Сергеев <sergeev.i@zpos.local>
2019-05-14 15:55:10 +0300

    Шаблон для страницы 404

--- lot.php
+++ lot.php
@@ -3,18 +3,23 @@ require_once(__DIR__ . '/inc/functions.php'); // Функции
 require_once(__DIR__ . '/inc/queries.php'); // Вызовы и подключения
 $configArr = require_once(__DIR__ . '/config/db.php'); // Конфигурация базы данных
 
-if (isset($_GET['pageId']) && !empty($_GET['pageId']))  {    
-  $dbConnect = getConn($configArr);
+$dbConnect = getConn($configArr);
+$categories = getCategories($dbConnect);
+
+if (isset($_GET['pageId']) && !empty($_GET['pageId']))  {   
   $pageId = mysqli_real_escape_string($dbConnect, $_GET['pageId']);
-  $categories = getCategories($dbConnect); 
   $lot = getLot($dbConnect, $pageId);
 
 } else {
-  http_response_code(404);
-  include_template('error.php', [
-    'categories' => $categories    
-  ]);
-  header("location: /pages/404.html");
+  http_response_code(404);   
+  $page_error = include_template('error.php', [
+    'categories' => $categories,
+    'title' => 'Страница не найдена',
+    'is_auth' => rand(0, 1),
+    'user_name' => 'Сергеев Игорь'  
+  ]);  
+  print($page_error);
+  die();
 }
 
 $page_content = include_template('detail_page.php', [
